3c817f3954beebd758651d3454c5faa41e31ba64
chirag-nyusoft <chirag.nyusoft@gmail.com>
2022-06-23 00:05:49 +0530

    addon-darkmode-functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
   return (
       <>
-       {/* <Navbar /> */}
-       <Navbar title="TextUtils" menu1="Home" menu2="About" menu3="Products" />
-       <div className="container">
-        <TextForm heading="Enter the analyze text:" />
+        {/* <Navbar /> */}
+        <Navbar title="TextUtils" menu1="Home" menu2="About" menu3="Products" />
+        <div className="container">
+          <TextForm heading="Enter the analyze text:" />
        </div>
+       <hr />
+       <About />
       </>
   );
 }
