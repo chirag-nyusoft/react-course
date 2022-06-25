c270abf08b77dd7c0b7ba3a523e44eec8747964c
chirag-nyusoft <chirag.nyusoft@gmail.com>
2022-06-25 14:54:38 +0530

    dark-mode-updated-25-06

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+  
   return (
       <>
         {/* <Navbar /> */}
-        <Navbar title="TextUtils" menu1="Home" menu2="About" menu3="Products" />
-        <div className="container">
-          <TextForm heading="Enter the analyze text:" />
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} menu1="Home" menu2="About" menu3="Products" />
+        <div className="container my-3">
+          <TextForm heading="Enter the analyze text:" mode={mode}/>
        </div>
        <hr />
        <About />
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,12 @@ export default function TextForm(props) {
     setText(e.target.value);
   }
 
+  const handleCopyText = () => {
+    let myText = document.getElementById('textword');
+    myText.select();
+    navigator.clipboard.writeText(myText.value);
+  }
+
   const handleUpCase = (param) => {
     var newText = '';
     
@@ -22,7 +28,12 @@ export default function TextForm(props) {
       case 'clear':
         newText = '';
         break;
-      
+
+      case 'spaces':
+        let myText = text.split(/[  ]+/);
+        newText = myText.join(" ");
+        break;
+
       default:
           break;
       }
@@ -31,20 +42,22 @@ export default function TextForm(props) {
 
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
       <h4>{props.heading}</h4>
       <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="textword" rows="8" cols="8" />
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="textword" rows="8" cols="8" style={{backgroundColor: props.mode === 'light' ? '#042743' : 'white', color: props.mode === 'light' ? 'white' : '#042743' }} />
       </div>
       <button className="btn btn-primary" onClick={() => handleUpCase('upper') }>UpperCase</button>
       <button className="btn btn-primary mx-2" onClick={() => handleUpCase('lower') }>LowerCase</button>
       <button className="btn btn-primary mx-2" onClick={() => handleUpCase('clear') }>Clear</button>      
+      <button className="btn btn-primary mx-2" onClick={() => handleCopyText() }>Copy Text</button>
+      <button className="btn btn-primary mx-2" onClick={() => handleUpCase('spaces') }>Remove Extra Spaces</button>
     </div>
-    <div className="container my-3">
-      <p>Total length of char is:: <span> {text.length} </span></p>
-      <p>Total words Are:: <span> { text.split(' ').length } </span></p>
-      <p>Reading time: <span> { 0.008 * text.split(' ').length } min... </span></p>
-      <p>Total vowels Are:: <span> { text.match(/[aeiou]/gi).length } </span> </p>
+    <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : '#042743'}}>
+      <p>Total length of char is:: <span> { text !== '' ? text.length : 0 } </span></p>
+      <p>Total words Are:: <span> { text !== '' ? text.split(' ').length : 0 } </span></p>
+      <p>Reading time: <span> { text !== '' ? 0.008 * text.split(' ').length : 0 } min... </span></p>
+      <p>Total vowels Are:: <span> { text !== '' ? text.match(/[aeiou]/gi).length : 0  } </span> </p>
     </div>
     </>
   )
